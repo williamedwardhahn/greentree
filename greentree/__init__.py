2c9e6f729d1a9dd5c7b7df656e4e2231d4eb4fda
William E Hahn <williamedwardhahn@gmail.com>
2020-07-01 02:48:14 -0400

    Update __init__.py

--- greentree/__init__.py
+++ greentree/__init__.py
@@ -905,6 +905,30 @@ def load_im(im):
     img_variable = torch.tensor(img_tensor).cuda(0)
     return img_variable
 
+def data_show(data_dir,batch=4):
+
+    data_transforms = {
+        'train': transforms.Compose([
+            transforms.RandomResizedCrop(224),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ]),
+        'val': transforms.Compose([
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ]),
+    }
+    
+    
+    image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x),data_transforms[x]) for x in ['train', 'val']}
+    dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=batch, shuffle=True, num_workers=4) for x in ['train', 'val']}
+    class_names = image_datasets['train'].classes
+
+    inputs, classes = next(iter(dataloaders['train']))
+    imshow(torchvision.utils.make_grid(inputs), title=[class_names[x] for x in classes])
 
 
 def inference(im,model):
