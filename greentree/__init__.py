2722fb4b546f94ec83ca43a9c5ade80fd622f3fd
William E Hahn <williamedwardhahn@gmail.com>
2020-07-01 02:23:29 -0400

    Update __init__.py

--- greentree/__init__.py
+++ greentree/__init__.py
@@ -588,14 +588,33 @@ def train_model(data_dir, num_epochs=25):
     model = models.resnet18(pretrained=True)
     num_ftrs = model.fc.in_features
     model.fc = nn.Linear(num_ftrs, 2)
-
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     model = model.to(device)
+    
+    # Data augmentation and normalization for training
+    # Just normalization for validation
+    data_transforms = {
+        'train': transforms.Compose([
+            transforms.RandomResizedCrop(224),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ]),
+        'val': transforms.Compose([
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ]),
+    }
+    
+    
     image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x),data_transforms[x]) for x in ['train', 'val']}
     dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=4, shuffle=True, num_workers=4) for x in ['train', 'val']}
     class_names = image_datasets['train'].classes
     dataset_sizes = {x: len(image_datasets[x]) for x in ['train', 'val']}
     
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    
     scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=8, gamma=0.05)
     optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
     criterion = nn.CrossEntropyLoss()
