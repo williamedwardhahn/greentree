3e0f64f574a86c0b180e45ecaca4aacdddeae669
William E Hahn <williamedwardhahn@gmail.com>
2020-07-01 02:31:54 -0400

    Update __init__.py

--- greentree/__init__.py
+++ greentree/__init__.py
@@ -732,7 +732,7 @@ def plot(x):
     
     
     
-def show_output(model, num_images=16):
+def show_output(model,data_dir, num_images=16):
     
     image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x),data_transforms[x]) for x in ['train', 'val']}
     dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=4, shuffle=True, num_workers=4) for x in ['train', 'val']}
