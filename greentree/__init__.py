58325ea5532b3ff081e8ad2d6db30e186df3e301
William E Hahn <williamedwardhahn@gmail.com>
2020-07-01 02:34:44 -0400

    Update __init__.py

--- greentree/__init__.py
+++ greentree/__init__.py
@@ -591,8 +591,7 @@ def train_model(data_dir, num_epochs=25):
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     model = model.to(device)
     
-    # Data augmentation and normalization for training
-    # Just normalization for validation
+   
     data_transforms = {
         'train': transforms.Compose([
             transforms.RandomResizedCrop(224),
@@ -733,6 +732,21 @@ def plot(x):
     
     
 def show_output(model,data_dir, num_images=16):
+    
+    data_transforms = {
+        'train': transforms.Compose([
+            transforms.RandomResizedCrop(224),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ]),
+        'val': transforms.Compose([
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ]),
+    }
     
     image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x),data_transforms[x]) for x in ['train', 'val']}
     dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=4, shuffle=True, num_workers=4) for x in ['train', 'val']}
